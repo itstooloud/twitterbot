ecd19ddf2d6347fc6725793736d00ca854bef759
Chris Collingwood <itstooloud@gmail.com>
2015-04-01 21:45:55 -0400

    Completed lesson. Some of the instruction may be outdated

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -26,9 +26,70 @@ class MicroBlogger
 
   def run
   	puts "Welcome to Your Twitter Interface!"
+    command = ""
+    while command != "q"
+      printf "Enter command : "
+      input = gets.chomp
+      parts = input.split(" ")
+      command = parts[0]
+      message = parts[1..-1].join(" ")
+      case command
+         when 'q' then puts "Goodbye!"
+         when 't' then tweet(parts[1..-1].join(" "))
+         when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+         else
+           puts "Sorry, I don't know how to #{command}"
+      end
+
+        
+    end
   	
+  end
+
+  def followers_list
+    screen_names = []
+    
+    @client.followers.each do |follower|
+      screen_names << @client.user(follower).screen_name
+    end
+    screen_names
+    
+  end
+
+  def spam_my_followers(message)
+
+    spam_list = followers_list
+    spam_list.each do |name|
+      dm(name, message)
+    end
+
+    
+  end
+
+  def everyones_last_tweet
+
+    friends = @client.friends
+    friends.each do |friend|
+      p friend.status.text
+    end
+    
+  end
+
+  def dm(target, message)
+
+    screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name }
+    if screen_names.include? target
+      puts "Trying to send #{target} this message: "
+      puts message
+      message = "d @#{target} #{message}"
+      tweet(message)
+    else
+      puts "That person isn't following you!"
+    end
+
+    
   end
 end
 
 blogger = MicroBlogger.new
-blogger.run
+p blogger.followers_list
